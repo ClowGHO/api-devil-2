a9c214939f554be61c8acd046ab44e6df0663e5b
dvlgaming <rajifhabibi56@gmail.com>
2022-02-13 20:53:43 +0700

    y

---
+++ canvas-tmp/ngetes.js
@@ -0,0 +1,5 @@
+const devil = "ini adalah folder sampah gambar canvas, pastikan output kesini"
+console.log(devil)
+
+// by viko dwi kurniawan- tempplate by zekais
+// saya bukan suhu lo ya awokawowkaowkoakwok jangan sok sok an bisa ngoding aslinya tampal punya orang
